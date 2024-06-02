fc7a321773bf015093d9f58f52127b591131557d
Martin <martin.thalmayr@gmail.com>
2024-06-02 21:17:26 +0200

    redid everything, as last time i apparently didnt push my actual code. Status ep 3 beginning

---
+++ LWGE/Editor/GameProject/CreateProject.cs
@@ -0,0 +1,163 @@
+﻿using Editor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Editor.GameProject
+{
+    [DataContract]
+    public class ProjectTemplate
+    {
+        [DataMember]
+        public string ProjectType { get; set; }
+        [DataMember]
+        public string ProjectFile { get; set; }
+        [DataMember]
+        public List<string> Folders { get; set; }
+
+        public byte[] Icon { get; set; }
+        public byte[] Screenshot { get; set; }
+        public string IconFilePath { get; set; }
+        public string ScreenshotFilePath { get; set; }
+        public string ProjectFilePath { get; set; }
+    }
+    class CreateProject : ViewModelBase
+    {
+        //TODO get path from installation
+        private readonly string _templatePath = @"..\..\Editor\Templates";
+
+        private string _projectName = "NewProject";
+        public string ProjectName
+        {
+            get => _projectName;
+            set
+            {
+                if (_projectName != value)
+                {
+                    _projectName = value;
+                    ValidateProjectPath();
+                    OnPropertyCanged(nameof(ProjectName));
+                }
+            }
+        }
+
+        private string _projectPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\LWGEProjects\";
+        public string ProjectPath
+        {
+            get => _projectPath;
+            set
+            {
+                if (_projectPath != value)
+                {
+                    _projectPath = value;
+                    ValidateProjectPath();
+                    OnPropertyCanged(nameof(ProjectPath));
+                }
+            }
+        }
+
+        private bool _isValid;
+        public bool IsValid
+        {
+            get => _isValid;
+            set
+            {
+                if (_isValid != value)
+                {
+                    _isValid = value;
+                    OnPropertyCanged(nameof(IsValid));
+                }
+            }
+        }
+
+        private string _errorMsg;
+        public string ErrorMsg
+        {
+            get => _errorMsg;
+            set
+            {
+                if (_errorMsg != value)
+                {
+                    _errorMsg = value;
+                    OnPropertyCanged(nameof(ErrorMsg));
+                }
+            }
+        }
+
+        private ObservableCollection<ProjectTemplate> _projectTemplates = new ObservableCollection<ProjectTemplate>();
+        public ReadOnlyObservableCollection<ProjectTemplate> ProjectTemplates
+        { get; }
+
+        private bool ValidateProjectPath()
+        {
+            var path = ProjectPath;
+            if (!Path.EndsInDirectorySeparator(path)) path += @"\";
+            path += $@"{ProjectName}\";
+
+            IsValid = false;
+            if(String.IsNullOrWhiteSpace(ProjectName.Trim()))
+            {
+                ErrorMsg = "Type in a project name.";
+            }
+            else if(ProjectName.IndexOfAny(Path.GetInvalidFileNameChars()) != -1)
+            {
+                ErrorMsg = "Invalid Character(s) used in project name";
+            }
+            else if (String.IsNullOrWhiteSpace(ProjectPath.Trim()))
+            {
+                ErrorMsg = "Type in a project path.";
+            }
+            else if (ProjectPath.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                ErrorMsg = "Invalid Character(s) used in project path";
+            }
+            else if (Path.Exists(path) && Directory.EnumerateDirectories(path).Any())
+            {
+                ErrorMsg = "Selected project already exists and is not empty.";
+            }
+            else
+            {
+                ErrorMsg = String.Empty;
+                IsValid = true;
+            }
+            return IsValid;
+        }
+
+        public CreateProject()
+        {
+            ProjectTemplates = new ReadOnlyObservableCollection<ProjectTemplate>(_projectTemplates);
+            try
+            {
+                var templateFiles = Directory.GetFiles(_templatePath, "template.xml", SearchOption.AllDirectories);
+                Debug.Assert(templateFiles.Any());
+                foreach (var file in templateFiles)
+                {
+                    var template = Serializer.FromFile<ProjectTemplate>(file);
+                    template.IconFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Icon.png"));
+                    template.Icon = File.ReadAllBytes(template.IconFilePath);
+                    template.ScreenshotFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), "Screenshot.png"));
+                    template.Screenshot = File.ReadAllBytes(template.ScreenshotFilePath);
+                    template.ProjectFilePath = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(file), template.ProjectFile));
+                    _projectTemplates.Add(template);
+                }
+                ValidateProjectPath();
+            }
+            catch(Exception ex) 
+            { 
+                Debug.WriteLine(ex.Message);
+                //TODO log errors
+            }
+
+            
+        }
+    }
+
+
+}
---
+++ LWGE/Editor/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Editor.GameProject;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectProwserDialog();
+        }
+
+        private void OpenProjectProwserDialog()
+        {
+            var projectBrowser = new ProjectBrowserDialog();
+            projectBrowser.Owner = this;
+            if(projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
